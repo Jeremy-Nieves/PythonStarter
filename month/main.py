cd98fa5a3726dd3c271fd560720150b54a76ae49
Jeremy-Nieves <jeremyjnieves@gmail.com>
2022-07-20 20:29:31 -0400

    Short calculation where I can reflect on how much money I am losing

---
+++ month/main.py
@@ -0,0 +1,10 @@
+def takehome():
+
+    num1 = int(input("What was your first check?"))
+    num2 = int(input("What was your second check"))
+    expenses = 2457
+    money = num1 + num2
+    total = money - expenses
+    print(total)
+
+takehome()
