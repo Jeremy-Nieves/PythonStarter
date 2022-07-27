417fc0053ee41b77d65a188e3ae1471c07c6e3ac
Jeremy-Nieves <jeremyjnieves@gmail.com>
2022-07-26 21:50:22 -0400

    Shortened Code

--- keys/main.py
+++ keys/main.py
@@ -15,43 +15,11 @@ majorkeydict = {
 
     }
 
-majorkey = str.lower(input("Enter the root note: "))
+root_note = str.lower(input("Enter the root note: "))
 
-if majorkey == "c":
-    print("The notes played in C Major are: ")
-    print(majorkeydict["c"])
-elif majorkey == "g":
-    print("The notes played in G Major are:")
-    print(majorkeydict["g"])
-elif majorkey == "d":
-    print("The notes played in D Major are:")
-    print(majorkeydict["d"])
-elif majorkey == "a":
-    print("The notes played in A Major are:")
-    print(majorkeydict["a"])
-elif majorkey == "e":
-    print("The notes played in E Major are:")
-    print(majorkeydict["e"])
-elif majorkey == "b":
-    print("The notes played in E Major are:")
-    print(majorkeydict["b"])
-elif majorkey == "gb":
-    print("The notes played in E Major are:")
-    print(majorkeydict["gb"])
-elif majorkey == "db":
-    print("The notes played in E Major are:")
-    print(majorkeydict["db"])
-elif majorkey == "ab":
-    print("The notes played in E Major are:")
-    print(majorkeydict["ab"])
-elif majorkey == "eb":
-    print("The notes played in E Major are:")
-    print(majorkeydict["eb"])
-elif majorkey == "bb":
-    print("The notes played in E Major are:")
-    print(majorkeydict["bb"])
-elif majorkey == "f":
-    print("The notes played in E Major are:")
-    print(majorkeydict["f"])
-else:
-    print("Not a key!")
+if root_note in majorkeydict:
+    notes = majorkeydict[root_note]
+    print(f"The notes in the key of {root_note} major are {notes}")
+
+
+
