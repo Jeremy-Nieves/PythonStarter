c9e7c0635a6a81933b4dcf143886902f2a136ecb
Jeremy-Nieves <jeremyjnieves@gmail.com>
2022-08-14 00:42:01 -0400

    Create characters in your RPG

---
+++ rpg_characters/main.py
@@ -0,0 +1,66 @@
+from random import randint
+from rpg_character_stats import attacker, defender, healer, monk, magician
+
+party_members = []
+party_stats = {}
+
+
+def create_party(size=1):
+
+    i = 0
+    while i < size:
+        set_job = randint(1, 5)
+
+        if set_job == 1:
+            rpg_class = 'attacker'
+            print('Attacker has the following base stats:')
+            for k, v in attacker.items():
+                print(k, v)
+
+        elif set_job == 2:
+            rpg_class = 'defender'
+            print('\nDefender has the following base stats:')
+            for k, v in defender.items():
+                print(k, v)
+
+        elif set_job == 3:
+            rpg_class = 'magician'
+            print('\nMagician has the following base stats: ')
+            for k, v in magician.items():
+                print(k, v)
+
+        elif set_job == 4:
+            rpg_class = 'monk'
+            print('\nMonk has the following base stats: ')
+            for k, v in monk.items():
+                print(k, v)
+
+        else:
+            rpg_class = 'healer'
+            print('\nHealer has the following base stats: ')
+            for k, v in healer.items():
+                print(k, v)
+
+        new_member = [rpg_class]
+        party_members.append(new_member)
+        i += 1
+
+
+create_party(3)
+print('\nYour party consists of the following roles: ')
+print(party_members)
+
+
+def name_party():
+    name_player_1 = input('What will you name your first party member: ')
+    party_members[0] = name_player_1
+
+    name_player_2 = input('What will you name your second party member: ')
+    party_members[1] = name_player_2
+
+    name_player_3 = input('What will you name your third party member: ')
+    party_members[2] = name_player_3
+
+    print(party_members)
+
+name_party()
---
+++ rpg_characters/rpg_character_stats.py
@@ -0,0 +1,77 @@
+from random import randint
+
+attacker = {
+
+    'HP:': 125,
+    'MP': 25,
+    'STR': 13,
+    'VIT': 12,
+    'DEX': 9,
+    'AGI': 9,
+    'INT': 7,
+    'MND': 6,
+    'LUCK': randint(4, 9),
+    'SPECIAL': 'critical hits allow attacker to attack again'
+
+    }
+
+defender = {
+
+    'HP:': 142,
+    'MP': 62,
+    'STR': 11,
+    'VIT': 17,
+    'DEX': 10,
+    'AGI': 7,
+    'INT': 8,
+    'MND': 11,
+    'LUCK': randint(5, 10),
+    'SPECIAL': '15% chance damage is reduced by 30%'
+
+    }
+
+healer = {
+
+    'HP:': 95,
+    'MP': 125,
+    'STR': 7,
+    'VIT': 6,
+    'DEX': 8,
+    'AGI': 10,
+    'INT': 11,
+    'MND': 15,
+    'LUCK': randint(6, 13),
+    'SPECIAL': '5% chance that all tiers of cure spells consume 0 MP'
+
+    }
+
+magician = {
+
+    'HP:': 102,
+    'MP': 137,
+    'STR': 5,
+    'VIT': 5,
+    'DEX': 6,
+    'AGI': 12,
+    'INT': 15,
+    'MND': 11,
+    'LUCK': randint(3, 7),
+    'SPECIAL': '1/10 of all damage is converted into MP'
+
+    }
+
+monk = {
+
+    'HP:': 150,
+    'MP': 0,
+    'STR': 13,
+    'VIT': 15,
+    'DEX': 11,
+    'AGI': 9,
+    'INT': 7,
+    'MND': 7,
+    'LUCK': randint(3, 7),
+    'SPECIAL': 'physical attack twice per turn'
+
+    }
+
